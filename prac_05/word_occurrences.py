f69b2a1566f5ef46107ca2d6ccd87b77a76dc997
Dani Ryland-Lawson <Thismygithub87>
2019-08-29 16:06:49 +1000

    New program that needs to count the number of words and print to the screen.

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,11 @@
+"""Word count occurrences in a string"""
+
+#Need to get input from the user and add to an empty dictionary.
+
+users_words = {}
+user_text = input(str("Please enter a string of text: "))
+
+words = user_text.split()
+
+
+
