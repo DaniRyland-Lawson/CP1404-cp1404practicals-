98cf495d1ee7db9dcaf1b96fa6c54f051a9b2d4a
Dani Ryland-Lawson <Thismygithub87>
2019-08-29 16:32:57 +1000

    Program now takes user input and counts the number of words on screen with word split.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -6,6 +6,19 @@ users_words = {}
 user_text = input(str("Please enter a string of text: "))
 
 words = user_text.split()
+#Counting the words from the users input
+for word in words:
+    word_occurrence = users_words.get(word, 0)
+    if word_occurrence is None:
+        users_words[word] = 1
+    else:
+        users_words[word] = word_occurrence + 1
+
+words = list(users_words.keys())
+words.sort()
+max_length = max((len(word) for word in words))
+for word in words:
+    print("{:{}} : {}".format(word, max_length, users_words[word]))
 
 
 
