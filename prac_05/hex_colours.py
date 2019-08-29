7b024a6a51ba5e57731da5d067c8936fc89df0c9
Dani Ryland-Lawson <Thismygithub87>
2019-08-29 14:40:39 +1000

    Realised, I didn't need a function. Just had to change code to read my dictionary.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,15 +1,17 @@
 """ Please choose a colour name and we will tell you the hexadecimal colour code."""
-HEX_COLOURS = {"black": "##000000", "blue1": "#0000ff", "DarkGreen": "#006400", "DarkOrange": "#ff8c00",
-               "DarkTurquoise": "#00ced1", "DeepPink1": "#ff1493", "DarkViolet": "#9400d3",
-               "firebrick1": "#ff3030", "ForestGreen": "#228b22", "DeepSkyBlue1": "#00bfff"}
+
+HEX_COLOURS = {"black": "#000000", "blue1": "#0000ff",
+               "darkgreen": "#006400", "darkorange": "#ff8c00",
+               "darkturquoise=": "#00ced1", "deeppink": "#ff1493",
+               "darkviolet": "#9400d3", "firebrick1": "#ff3030",
+               "forestgreen": "#228b22", "deepskyblue1": "#00bfff"}
 # print(HEX_COLOURS)
 
 
 colour_name = input("Enter colour name: ").upper()
 while colour_name != "":
     if colour_name in HEX_COLOURS:
-        print(colour_name, "is", HEX_COLOURS[colour_name])
+        print("The code for \"{}\" is {}".format(colour_name, HEX_COLOURS[colour_name]))
     else:
-        print("Invalid colour name")
-    colour_name = input("Enter colour name: ").upper()
-
+        print("Invalid entry")
+    colour_name = input("Enter colour name: ")
