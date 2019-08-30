27f5670c42657feafac973194d03d58ce8e96bfe
Dani Ryland-Lawson <Thismygithub87>
2019-08-30 12:23:30 +1000

    Changing program to have main() and leaving the user with an exit message.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -5,13 +5,19 @@ HEX_COLOURS = {"black": "#000000", "blue1": "#0000ff",
                "darkturquoise=": "#00ced1", "deeppink": "#ff1493",
                "darkviolet": "#9400d3", "firebrick1": "#ff3030",
                "forestgreen": "#228b22", "deepskyblue1": "#00bfff"}
+
+
 # print(HEX_COLOURS)
-
-
-colour_name = input("Enter colour name: ")
-while colour_name != "":
-    if colour_name in HEX_COLOURS:
-        print("The code for \"{}\" is {}".format(colour_name, HEX_COLOURS[colour_name]))
-    else:
-        print("Invalid entry")
+def main():
     colour_name = input("Enter colour name: ")
+    while colour_name != "":
+        if colour_name in HEX_COLOURS:
+            print("The code for \"{}\" is {}".format(colour_name, HEX_COLOURS[colour_name]))
+        else:
+            print("Invalid entry")
+        colour_name = input("Enter colour name: ")
+
+    print("GoodBye!")
+
+
+main()
