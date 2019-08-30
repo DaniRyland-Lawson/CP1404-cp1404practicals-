1477cfda89de0b529d6525014a8355bbd7acceb1
Dani Ryland-Lawson <Thismygithub87>
2019-08-30 10:46:49 +1000

    Refactoring variable names as names are confusing. Removing "input(str", as not needed and is default string.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,20 +2,20 @@
 
 # Need to get input from the user and add to an empty dictionary.
 
-users_words = {}
-user_text = input(str("Please enter a string of text: "))
+user_string = {}
+user_text = input("Please enter a string of text: "))
 
 words = user_text.split()
 # Counting the words from the users input
 for word in words:
-    word_occurrence = users_words.get(word, 0)
-    if word_occurrence is None:
-        users_words[word] = 1
+    occurrence = user_string.get(word, 0)
+    if occurrence is None:
+        user_string[word] = 1
     else:
-        users_words[word] = word_occurrence + 1
+        user_string[word] = occurrence + 1
 
-words = list(users_words.keys())
+words = list(user_string.keys())
 words.sort()
 max_length = max((len(word) for word in words))
 for word in words:
-    print("{:{}} : {}".format(word, max_length, users_words[word]))
+    print("{:{}} : {}".format(word, max_length, user_string[word]))
