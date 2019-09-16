3c2e00e8dfeab65085999512c74f962de5944cec
Dani Ryland-Lawson <Thismygithub87>
2019-09-16 17:34:39 +1000

    Loading drink list from csv file Stripping and slipping the file

--- prac_06/evening_out.py
+++ prac_06/evening_out.py
@@ -1,18 +1,23 @@
 """Program to keep track of appropriate drinking"""
 
-from prac_06.drink import Drink
-from prac_06.drinklist import DrinkList
+from drink import Drink
+from drinklist import DrinkList
 
 
 def main():
     available_drinks = load_drinks("all_drinks.csv")
+    print([str(drink) for drink in available_drinks])
+
 
 def load_drinks(filename):
+    """Loading drink list from csv file"""
     all_drinks = []
     input_file = open(filename)
     for line in input_file:
+        """Stripping the line of the /n and splitting it at "," commas."""
         parts = line.strip().split(",")
-        print(parts)
+        # print(parts)
+        all_drinks.append(Drink(parts[0], float(parts[1]), float(parts[2]), float(parts[3])))
     input_file.close()
     return all_drinks
 
