1f776fad14a7997b17681148515ec0a941da23b7
Dani Ryland-Lawson <Thismygithub87>
2019-09-16 11:17:50 +1000

    Adding guitars and printing to screen

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,4 +1,18 @@
 """ CP1404 Programming II
 Guitars"""
 
+from prac_06.guitar import Guitar
+
+
 def main():
+    guitars = []
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(guitar_to_add)
+
+
+main()
