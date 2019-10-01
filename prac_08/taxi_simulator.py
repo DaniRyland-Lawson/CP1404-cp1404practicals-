165fabc1efe4d0f3cdd99a7ba635c95c09e6c468
Dani Ryland-Lawson <Thismygithub87>
2019-10-01 21:01:50 +1000

    Create new program for Taxis.

---
+++ prac_08/taxi_simulator.py
@@ -0,0 +1,17 @@
+"""CP1404 Programming II
+Prac 08 Taxi Simulator"""
+
+from prac_08.taxi import Taxi
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+
+def main():
+    total_cost = 0
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    print("Let's drive!")
+    print(MENU)
+
+
+main()
