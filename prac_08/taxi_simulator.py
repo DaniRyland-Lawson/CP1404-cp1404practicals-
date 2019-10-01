332ccab66a3b7ca0dab7147cf45e31212d7e06f8
Dani Ryland-Lawson <Thismygithub87>
2019-10-01 21:53:23 +1000

    Program no working with D drive.

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -8,6 +8,7 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
+
     total_cost = 0
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
@@ -21,13 +22,28 @@ def main():
             taxi_choice = int(input("Choose Taxi: "))
             current_taxi = taxis[taxi_choice]
             print("Bill to date: ${:.2f}".format(taxis[taxi_choice].get_fare()))
-        elif user_choice == "D":
-            pass
 
-    else:
-        print("Invalid Menu selection")
+        elif user_choice == "D":
+            current_taxi.start_fare()
+            distance_driven = float("Drive how far? ")
+            current_taxi.drive(distance_driven)
+            trip_cost = current_taxi.get_fare()
+            print("Your {} trip cost you ${:.2f}".format(current_taxi.name, trip_cost))
+            total_cost += trip_cost
+
+        else:
+            print("Invalid Menu selection")
+        print(MENU)
+        user_choice = input(">>>").upper()
+
+    print("Total trip cost: ${:.2f}".format(total_cost))
+    print("Taxi's are now:")
+    list_taxis(taxis)
+
 
 def list_taxis(taxis):
     for i, taxi in enumerate(taxis):
         print("{} - {}".format(i, taxi))
+
+
 main()
