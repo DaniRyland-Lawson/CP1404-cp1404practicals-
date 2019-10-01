95321206d1a44a3e6d0dab3533be7a66b4c393c5
Dani Ryland-Lawson <Thismygithub87>
2019-10-01 15:24:20 +1000

    Test for new_taxi

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -2,8 +2,17 @@
 Test Taxi Prac."""
 
 
-from prac_08.car import Car
 from prac_08.taxi import Taxi
 
 
+def main():
+    new_taxi = Taxi("Prius 1", 100, price_per_km=1.23)
+    new_taxi.drive(40)
+    print(new_taxi)
 
+    new_taxi.start_fare()
+    new_taxi.drive(100)
+    print(new_taxi)
+
+
+main()
