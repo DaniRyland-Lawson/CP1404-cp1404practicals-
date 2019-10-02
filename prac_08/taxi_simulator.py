d542e373f352fd1325efdeb8424ef4de2bace8d0
Dani Ryland-Lawson <Thismygithub87>
2019-10-02 11:16:31 +1000

    Set the current_taxi to default settings so if user chose D from menu, there is no crash.

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -8,7 +8,6 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
-
     total_cost = 0
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's drive!")
@@ -20,17 +19,15 @@ def main():
             print("Taxi's available:")
             list_taxis(taxis)
             taxi_choice = int(input("Choose Taxi: "))
-            current_taxi = taxis[taxi_choice]
             print("Bill to date: ${:.2f}".format(taxis[taxi_choice].get_fare()))
-
         elif user_choice == "D":
+            current_taxi = Taxi(None, 0)
             current_taxi.start_fare()
-            distance_driven = float("Drive how far? ")
+            distance_driven = float(input("Drive how far? "))
             current_taxi.drive(distance_driven)
             trip_cost = current_taxi.get_fare()
-            print("Your {} trip cost you ${:.2f}".format(current_taxi.name, trip_cost))
             total_cost += trip_cost
-
+            print("Your {} trip cost you ${:.2f}".format(current_taxi.name, trip_cost))
         else:
             print("Invalid Menu selection")
         print(MENU)
@@ -41,7 +38,7 @@ def main():
     list_taxis(taxis)
 
 
-def list_taxis(taxis):
+def list_taxis(taxis, ):
     for i, taxi in enumerate(taxis):
         print("{} - {}".format(i, taxi))
 
