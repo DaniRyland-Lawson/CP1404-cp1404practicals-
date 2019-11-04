a60238b318041e4679da9a7676085afe7eb9d9ee
Dani Ryland-Lawson <Thismygithub87>
2019-11-04 11:24:46 +1000

    Loop to rename files

--- prac_09/os_demo.py
+++ prac_09/os_demo.py
@@ -38,7 +38,7 @@ def main():
         # os.rename(filename, new_name)
 
         # Option 2: move file to new place, with new name
-        # shutil.move(filename, 'temp/' + new_name)
+        shutil.move(filename, 'temp/' + new_name)
 
 
 def get_fixed_filename(filename):
@@ -57,6 +57,10 @@ def demo_walk():
         print("(Current working directory is: {})".format(os.getcwd()))
 
         # TODO: add a loop to rename the files
+        for filename in filenames:
+            full_name = os.path.join(directory_name, filename)
+            new_name = os.path.join(directory_name, get_fixed_filename(filename))
+            os.rename(full_name, new_name)
 
 
 
