9fb0d794faef2740ad55a31bcda6ce0e201211da
Dani Ryland-Lawson <Thismygithub87>
2019-11-04 13:04:12 +1000

    Move files in different file extension directories.

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -1,2 +1,23 @@
 """CP1404 - Programming II Prac 9- Sorting files """
 
+import os
+
+
+def main():
+    """Sort files into folders with the same file extension."""
+    os.chdir("FilesToSort")
+    for file_name in os.listdir('.'):
+        if os.path.isdir(file_name):
+            continue
+
+        file_extension = file_name.split('.')[-1]
+        try:
+            os.mkdir(file_extension)
+        except FileExistsError:
+            pass
+
+        print("{}/{}".format(file_extension, file_name))
+        os.rename(file_name, "{}/{}".format(file_extension, file_name))
+
+
+main()
